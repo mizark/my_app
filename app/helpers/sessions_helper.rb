7a55d13e138bff462b8def99294685e19fc2e654
mizark <mja316@gmail.com>
2013-02-13 02:26:23 -0500

    Finish updating users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
 	def sign_in(user)
 		#Sign the user in
-		cookies.permanent[:remember_token] = { value: user.remember_token}
+		cookies.permanent[:remember_token] = user.remember_token
 		self.current_user = user
 
 	end
@@ -20,8 +20,27 @@ module SessionsHelper
 		@current_user = @current_user || User.find_by_remember_token(cookies[:remember_token])
 	end
 
+	def current_user?(user)
+		user == current_user
+	end
+
 	def sign_out
 		self.current_user = nil
 		cookies.delete(:remember_token)
 	end
+
+	def store_location
+		#session is like a cookie but expires on browswer close
+		session[:return_to] = request.fullpath
+
+	end
+
+	def redirect_back_or(default)
+
+		# if return_to key doesn't exist, will redirect to default
+		redirect_to(session[:return_to] || default )
+		session.delete(:return_to)
+	end
+
+
 end
