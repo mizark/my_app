5789bf611e4a8a22b168447db43ac3cff1ed0d98
mizark <mja316@gmail.com>
2013-03-05 04:48:22 -0500

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 MyApp::Application.routes.draw do
-  resources :users
+  
+
+  resources :users do
+    member do
+      #below are the names of the actions and the type of http request
+      get :following, :followers
+    end
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
+
 
   root to: 'static_pages#home'
 
